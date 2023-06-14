980ee51e39dabbf1873879b607df0dcadbdd6864
Alex Shpyrkovich <aliaksandr.shpyrkovich@insite.ai>
2023-06-14 20:49:36 +0200

    updated with camera

---
+++ index.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const video = document.querySelector('body > video');
+    const cvs = document.createElement('canvas');
+    const ctx = cvs.getContext('2d');
+  
+    const getMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia;
+  
+    if (getMedia) {
+      getMedia.call(navigator, { video: true, audio: false },
+        (stream) => {
+          if ('srcObject' in video) {
+            video.srcObject = stream;
+          } else {
+            const vu = window.URL || window.webkitURL;
+            video.src = vu.createObjectURL(stream);
+          }
+          video.play();
+        },
+        (error) => {
+          if (window.console) {
+            console.error(error);
+          }
+        }
+      );
+    }
+  
+    video.addEventListener('canplay', (ev) => {
+      if (!streaming) {
+        const width = 640; // Set your desired width
+        const height = video.videoHeight / (video.videoWidth / width);
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+  });
+  

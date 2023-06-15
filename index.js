f84b90dddae7e5a04a91c445c57ad44f8c7a5406
Alex Shpyrkovich <aliaksandr.shpyrkovich@insite.ai>
2023-06-15 09:36:53 +0200

    asdqweasd

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const video = document.querySelector("body > video");
-  const cvs = document.createElement("canvas");
-  const ctx = cvs.getContext("2d");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const video = document.querySelector("body > video");
+//   const cvs = document.createElement("canvas");
+//   const ctx = cvs.getContext("2d");
 
-  const getMedia =
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
+//   const getMedia =
+//     navigator.getUserMedia ||
+//     navigator.webkitGetUserMedia ||
+//     navigator.mozGetUserMedia ||
+//     navigator.msGetUserMedia;
 
-  if (getMedia) {
-    getMedia.call(
-      navigator,
-      {
-        video: {
-          width: 1280,
-          height: 720,
-        },
-        audio: false,
-        facingMode: {
-            exact: 'environment'
-          }
-      },
-      (stream) => {
-        if ("srcObject" in video) {
-          video.srcObject = stream;
-        } else {
-          const vu = window.URL || window.webkitURL;
-          video.src = vu.createObjectURL(stream);
-        }
-        video.play();
-      },
-      (error) => {
-        if (window.console) {
-          console.error(error);
-        }
-      }
-    );
-  }
+//   if (getMedia) {
+//     getMedia.call(
+//       navigator,
+//       {
+//         video: {
+//           width: 1280,
+//           height: 720,
+//         },
+//         audio: false,
+//         facingMode: {
+//             exact: 'environment'
+//           }
+//       },
+//       (stream) => {
+//         if ("srcObject" in video) {
+//           video.srcObject = stream;
+//         } else {
+//           const vu = window.URL || window.webkitURL;
+//           video.src = vu.createObjectURL(stream);
+//         }
+//         video.play();
+//       },
+//       (error) => {
+//         if (window.console) {
+//           console.error(error);
+//         }
+//       }
+//     );
+//   }
 
-  video.addEventListener(
-    "canplay",
-    (ev) => {
-      if (!streaming) {
-        const width = 640; // Set your desired width
-        const height = video.videoHeight / (video.videoWidth / width);
-        video.setAttribute("width", width);
-        video.setAttribute("height", height);
-        streaming = true;
-      }
-    },
-    false
-  );
-});
+//   video.addEventListener(
+//     "canplay",
+//     (ev) => {
+//       if (!streaming) {
+//         const width = 640; // Set your desired width
+//         const height = video.videoHeight / (video.videoWidth / width);
+//         video.setAttribute("width", width);
+//         video.setAttribute("height", height);
+//         streaming = true;
+//       }
+//     },
+//     false
+//   );
+// });
+
+window.addEventListener("load", () => {
+    var modelViewer = document.querySelector('model-viewer');
+    console.log("Can activate AR: " + modelViewer.canActivateAR);
+    modelViewer.activateAR();
+  });
